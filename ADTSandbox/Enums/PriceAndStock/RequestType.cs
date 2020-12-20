99ea1d21451ec7f4fb2c8ed5ccf113d5c0d58b52
Dennis Renz <dennis@r-renz.de>
2020-12-20 13:00:18 +0100

    Add request type enum, implement filter

---
+++ ADTSandbox/Enums/PriceAndStock/RequestType.cs
@@ -0,0 +1,18 @@
+﻿namespace ADTSandbox.Enums.PriceAndStock
+{
+    /// <summary>
+    /// The type of the request.
+    /// </summary>
+    public enum RequestType
+    {
+        /// <summary>
+        /// A request for a single article.
+        /// </summary>
+        SingleArticle,
+
+        /// <summary>
+        /// A request for all articles
+        /// </summary>
+        AllArticles
+    }
+}
--- ADTSandbox/Filters/PriceAndStockRequestFilter.cs
+++ ADTSandbox/Filters/PriceAndStockRequestFilter.cs
@@ -1,6 +1,31 @@
-﻿namespace ADTSandbox.Filters
+﻿using ADTSandbox.Enums.PriceAndStock;
+
+namespace ADTSandbox.Filters
 {
+    /// <summary>
+    /// The filter to construct a price and stock request from.
+    /// </summary>
     public record PriceAndStockRequestFilter
     {
+        /// <summary>
+        /// The articles' sku. Needs to be set if <see cref="RequestType"/>
+        /// is <see cref="RequestType.SingleArticle"/>, is ignored otherwise.
+        /// </summary>
+        public string? Sku { get; init; }
+
+        /// <summary>
+        /// The request type. Available values are:
+        /// <list type="bullet">
+        /// <item>
+        ///     <term><see cref="RequestType.SingleArticle"/></term>
+        ///     <description>A request for a single article.</description>
+        /// </item>
+        /// <item>
+        ///     <term><see cref="RequestType.AllArticles"/></term>
+        ///     <description>A request for all articles.</description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        public RequestType RequestType { get; init; }
     }
 }
