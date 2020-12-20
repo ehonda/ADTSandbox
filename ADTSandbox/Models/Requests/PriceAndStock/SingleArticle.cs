0b9903b78cad7e82992792c5d17bcb920009e8a3
Dennis Renz <dennis@r-renz.de>
2020-12-20 18:03:39 +0100

    Remove explicit cast, use copy constructor instead

--- ADTSandbox/Models/Requests/PriceAndStock/SingleArticle.cs
+++ ADTSandbox/Models/Requests/PriceAndStock/SingleArticle.cs
@@ -3,8 +3,27 @@
     /// <inheritdoc cref="Base.SingleArticle"/>
     public record SingleArticle : Base.SingleArticle, IPriceAndStockRequest
     {
+        /// <summary>
+        /// The 0-ary constructor.
+        /// </summary>
+        public SingleArticle()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// 1-ary constructor, to construct a <see cref="SingleArticle"/>
+        /// from a <see cref="Base.SingleArticle"/> by calling the
+        /// base copy constructor.
+        /// </summary>
+        /// <param name="singleArticle">The base single article to construct from.</param>
+        public SingleArticle(Base.SingleArticle singleArticle)
+            : base(singleArticle)
+        {
+        }
+
         /// <inheritdoc cref="Base.SingleArticle.FromFilterData(string?)"/>
         public static new SingleArticle FromFilterData(string? sku)
-            => (SingleArticle)Base.SingleArticle.FromFilterData(sku);
+            => new(Base.SingleArticle.FromFilterData(sku));
     }
 }
