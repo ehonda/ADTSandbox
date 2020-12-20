00186470bcd75f266c5980e4ef400e972cbc8bf1
Dennis Renz <dennis@r-renz.de>
2020-12-20 19:06:20 +0100

    Add tests for valid requests

--- ADTSandbox.Tests/PriceAndStockRequestTest.cs
+++ ADTSandbox.Tests/PriceAndStockRequestTest.cs
@@ -52,5 +52,32 @@ namespace ADTSandbox.Tests
                     Sku = null,
                     RequestType = RequestType.SingleArticle
                 }));
+
+        [Test]
+        public void PriceAndStockRequest_From_SingleArticle_Filter_With_Sku_Sets_That_Sku()
+        {
+            var filter = new PriceAndStockRequestFilter
+            {
+                Sku = "100",
+                RequestType = RequestType.SingleArticle
+            };
+            var request = IPriceAndStockRequest.FromFilter(filter);
+
+            var expectedRequest = new SingleArticle { Sku = filter.Sku };
+            Assert.That(request, Is.EqualTo(expectedRequest));
+        }
+
+        [Test]
+        public void PriceAndStockRequest_From_AllArticles_Filter_Needs_No_Sku_Set()
+        {
+            var request = IPriceAndStockRequest.FromFilter(new PriceAndStockRequestFilter
+            {
+                Sku = null,
+                RequestType = RequestType.AllArticles
+            });
+
+            var expectedRequest = new AllArticles();
+            Assert.That(request, Is.EqualTo(expectedRequest));
+        }
     }
 }
