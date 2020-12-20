7a1f0c3cd0ab3ddd17e7ea71aa216d85a8569a7f
Dennis Renz <dennis@r-renz.de>
2020-12-20 13:46:03 +0100

    Complete FromFilter implementation

--- ADTSandbox/Models/Requests/PriceAndStock/IPriceAndStockRequest.cs
+++ ADTSandbox/Models/Requests/PriceAndStock/IPriceAndStockRequest.cs
@@ -9,12 +9,27 @@ namespace ADTSandbox.Models.Requests.PriceAndStock
     /// </summary>
     public interface IPriceAndStockRequest
     {
+        /// <summary>
+        /// Creates a <see cref="IPriceAndStockRequest"/> from the specified
+        /// <paramref name="filter"/> if it is set correctly, throws an exception otherwise.
+        /// </summary>
+        /// <param name="filter">The filter to create the request from.</param>
+        /// <returns>The validated request.</returns>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown if <paramref name="filter"/> is <c>null</c>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///     Thrown if property <see cref="PriceAndStockRequestFilter.RequestType"/>
+        ///     of <paramref name="filter"/> contains a value that is does not represent
+        ///     a valid request type.
+        /// </exception>
         public static IPriceAndStockRequest FromFilter(PriceAndStockRequestFilter? filter)
             => filter?.RequestType switch
             {
                 null => throw new ArgumentNullException(nameof(filter)),
                 RequestType.SingleArticle => SingleArticle.FromFilterData(filter.Sku),
-                RequestType.AllArticles => new AllArticles()
+                RequestType.AllArticles => new AllArticles(),
+                _ => throw new ArgumentException($"Unkown RequestType enum value: {filter.RequestType}")
             };
     }
 }
