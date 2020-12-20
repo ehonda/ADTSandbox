88cb29f6a52ed178d86b55ea969e5ea172716bc8
Dennis Renz <dennis@r-renz.de>
2020-12-20 18:39:45 +0100

    Add test for null filter

--- ADTSandbox.Tests/PriceAndStockRequestTest.cs
+++ ADTSandbox.Tests/PriceAndStockRequestTest.cs
@@ -1,4 +1,6 @@
+using ADTSandbox.Requests.PriceAndStock.RequestTypes;
 using NUnit.Framework;
+using System;
 
 namespace ADTSandbox.Tests
 {
@@ -7,6 +9,29 @@ namespace ADTSandbox.Tests
         [SetUp]
         public void Setup()
         {
+        }
+
+        [Test]
+        public void SingleArticle_Request_Is_Default_Constructible_With_Empty_Sku()
+        {
+            var request = new SingleArticle();
+            Assert.That(request.Sku, Is.EqualTo(string.Empty));
+        }
+
+        [Test]
+        public void SingleArticle_Request_Is_Copy_Constructible()
+        {
+            var request = new SingleArticle();
+            var copiedRequest = new SingleArticle(request);
+            Assert.That(request, Is.EqualTo(copiedRequest));
+        }
+
+        // TODO: How do we deal with null skus?
+
+        [Test]
+        public void PriceAndStockRequest_From_Null_Filter_Throws()
+        {
+            Assert.Catch<ArgumentNullException>(() => IPriceAndStockRequest.FromFilter(null));
         }
     }
 }
